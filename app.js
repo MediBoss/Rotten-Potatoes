b6de49b491e42004043ea28ecfd9c573f2b6b362
MediBoss <mediassumani49@gmail.com>
2018-09-01 17:40:52 -0700

    added MongoDB and Review Model

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
+    // importing necessary modules
 const express = require('express');
-var exphds = require('express-handlebars');
+const mongoose = require('mongoose');
 const app = express();
+var exphds = require('express-handlebars');
 
+  // setting up the connection to the data base
+mongoose.connect('mongodb://localhost/rotten-potatoes')
+
+  // setting up client's views
 app.engine('handlebars', exphds({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
+  // Model
+  const Review = mongoose.model('Review', {
+    title: String
+  });
 
 // Mock array of reviews
-let reviews = [
-  { title: "Great Review"},
-  { title: "Next Review"}
-];
 
-// The root route
+// let reviews = [
+//   { title: "Great Review"},
+//   { title: "Next Review"},
+//   { title: "Better Review"}
+// ];
+
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews});
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(error => {
+      console.log(error);
+    })
 })
 
 app.listen(3000, () => {
