d495cbda2e3fc705b988f42be81f149142c1b809
MediBoss <mediassumani49@gmail.com>
2018-09-02 17:11:04 -0700

    Added CREATE route and added attributes

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
     // importing necessary modules
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 var exphds = require('express-handlebars');
 
 // Express App instance
 const app = express();
 // connecting to the database
-mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({ extended: true}));
 
   // setting up client's views
 app.engine('handlebars', exphds({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-  // Model
+  // MODEL
   const Review = mongoose.model('Review', {
-    title: String
+    title: String,
+    description: String,
+    movieTitle: String,
   });
 
-// Mock array of reviews
-
-// let reviews = [
-//   { title: "Great Review"},
-//   { title: "Next Review"},
-//   { title: "Better Review"}
-// ];
-
+// ROUTE : INDEX
 app.get('/', (req, res) => {
   Review.find().then(reviews => {
       res.render('reviews-index', { reviews: reviews });
@@ -32,6 +29,21 @@ app.get('/', (req, res) => {
     .catch(error => {
       console.log(error);
     })
+})
+
+// ROUTE : NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+// ROUTE : CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.listen(3000, () => {
