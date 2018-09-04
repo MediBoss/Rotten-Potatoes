16e14346eb750e3d86922060d10e38f08b880486
MediBoss <mediassumani49@gmail.com>
2018-09-04 10:13:52 -0700

    user can destroy a review

--- app.js
+++ app.js
@@ -77,6 +77,17 @@ app.put('/reviews/:id', (req, res) => {
 })
 
 
+// ROUTE : DELETE
+app.delete('/reviews/:id', (req, res) => {
+  console.log('DELETE Review')
+  Review.findByIdAndRemove(req.params.id).then( review => {
+    res.redirect('/');
+  }).catch( err => {
+    console.log(err.message);
+  })
+
+})
+
 app.listen(3000, () => {
   console.log('App listening on Port 3000')
 })
