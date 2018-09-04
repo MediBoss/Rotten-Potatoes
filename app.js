63d006ef6ebff008fd9be0b8b48f181f909865fa
MediBoss <mediassumani49@gmail.com>
2018-09-03 21:24:50 -0700

    user can edit and update reviews

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // MONGODB CONNECTION
 mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
   // VIEWS SET UP
 let exphds = require('express-handlebars');
@@ -40,7 +40,7 @@ app.get('/reviews/new', (req, res) => {
 
 // ROUTE : CREATE
 app.post('/reviews', (req, res) => {
-  Review.create(req.body).then((review) => {
+  Review.create(req.body).then(review => {
     console.log(review);
     res.redirect(`/reviews/${review._id}`)
   }).catch((err) => {
@@ -49,16 +49,17 @@ app.post('/reviews', (req, res) => {
 })
 
 // ROUTE : SHOW
-app.get('/revies/:id', (req, res) => {// fix that
-  Review.findById(req.params.id).then((review) => {
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then(review => {
     res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
 })
 
+
 // ROUTE : EDIT
-app.get('/reviews/:id/edit' function (req, res) {
+app.get('/reviews/:id/edit', (req, res) => {
   Review.findById(req.params.id, function(err, review) {
     res.render('reviews-edit', { review: review});
   })
@@ -74,6 +75,7 @@ app.put('/reviews/:id', (req, res) => {
     console.log(err.message)
   })
 })
+
 
 app.listen(3000, () => {
   console.log('App listening on Port 3000')
