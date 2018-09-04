da034416a5b38bccc7af0d4959f964b4af321116
MediBoss <mediassumani49@gmail.com>
2018-09-03 19:32:12 -0700

    added update and edit routes

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 
 // MONGODB CONNECTION
 mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(methodOverride('_method'))
 
   // VIEWS SET UP
 let exphds = require('express-handlebars');
@@ -52,6 +54,24 @@ app.get('/revies/:id', (req, res) => {// fix that
     res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
+  })
+})
+
+// ROUTE : EDIT
+app.get('/reviews/:id/edit' function (req, res) {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', { review: review});
+  })
+})
+
+
+// ROUTE : UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body).then(review => {
+    res.redirect(`/reviews/${review._id}`)
+  })
+  .catch(err => {
+    console.log(err.message)
   })
 })
 
