e864c82c92784e18038555ba3d796611d1ccdda4
MediBoss <mediassumani49@gmail.com>
2018-09-04 10:28:42 -0700

    fixed issue with delete route

--- app.js
+++ app.js
@@ -27,67 +27,67 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   Review.find().then(reviews => {
       res.render('reviews-index', { reviews: reviews });
-    })
-    .catch(error => {
+    }).catch(error => {
       console.log(error);
-    })
-})
+    });
+});
 
 // ROUTE : NEW
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
-})
+});
 
 // ROUTE : CREATE
 app.post('/reviews', (req, res) => {
-  Review.create(req.body).then(review => {
+  Review.create(req.body).then( review => {
     console.log(review);
     res.redirect(`/reviews/${review._id}`)
   }).catch((err) => {
     console.log(err.message);
-  })
-})
+  });
+});
 
 // ROUTE : SHOW
 app.get('/reviews/:id', (req, res) => {
-  Review.findById(req.params.id).then(review => {
+  Review.findById(req.params.id).then( review => {
     res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
-  })
-})
+  });
+});
 
 
 // ROUTE : EDIT
 app.get('/reviews/:id/edit', (req, res) => {
   Review.findById(req.params.id, function(err, review) {
     res.render('reviews-edit', { review: review});
-  })
-})
+  });
+});
 
 
 // ROUTE : UPDATE
 app.put('/reviews/:id', (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body).then(review => {
-    res.redirect(`/reviews/${review._id}`)
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-})
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    }).catch(err => {
+      console.log(err.message)
+    });
+});
 
 
 // ROUTE : DELETE
 app.delete('/reviews/:id', (req, res) => {
   console.log('DELETE Review')
-  Review.findByIdAndRemove(req.params.id).then( review => {
-    res.redirect('/');
-  }).catch( err => {
-    console.log(err.message);
-  })
+  Review.findByIdAndRemove(req.params.id)
+    .then(review => {
+      res.redirect('/');
+    }).catch(err => {
+      console.log(err.message);
+  });
+});
 
-})
-
+// LISTENING
 app.listen(3000, () => {
   console.log('App listening on Port 3000')
-})
+});
