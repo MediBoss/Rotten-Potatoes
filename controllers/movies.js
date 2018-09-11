3b874f192aa0a0c1ab7ee4d404825c4da4c8fc3b
MediBoss <mediassumani49@gmail.com>
2018-09-11 12:13:16 -0700

    adding movie template

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const http = require('http');
 const MovieDb = require('moviedb-promise');
+const apiKey = '0cc67b6fe76b6e66f651671d7509fd3c'
 const moviedb = new MovieDb('0cc67b6fe76b6e66f651671d7509fd3c');
 
-//ROUTE : INDEX
+// ROUTE : INDEX
 router.get('/', (req, res) => {
   moviedb.miscNowPlayingMovies()
     .then(response => {
-      res.render('movies-index', { movies: response.results });
+      res.render('movies-index', { movies: response.results })
     }).catch(console.error)
 });
 
 // ROUTE : SHOW
+router.get('/movies/:id', (req, res) => {
+  moviedb.getInfo({
+    movieId: req.params.id
+})
 
 module.exports = router;
