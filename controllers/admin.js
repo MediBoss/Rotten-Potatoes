810bee4c4783dedfa2fa8dc1d125a85db79e994f
MediBoss <mediassumani49@gmail.com>
2018-09-15 16:02:32 -0700

    adding admin.js

---
+++ controllers/admin.js
@@ -0,0 +1,19 @@
+
+// I NEED TO FIX THIS
+
+const express = require('express');
+const router = express.Router();
+
+// PATH TO REVIEW MODEL
+const Review = require('../models/review');
+
+router.get('/admin', (req, res) => {
+    Review.find().then( reviews => {
+        res.render('admin', { reviews: reviews });
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+module.exports = router;
+
