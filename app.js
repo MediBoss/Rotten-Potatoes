ead0f2d899711c4cfcddad5842c49010f9229068
Medi Assumani <mediassumani49@gmail.com>
2018-09-22 14:43:05 -0700

    successfully added admin dashboard

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 
-    // IMPORTING MODULES NEEDED
+// IMPORTING EXTERNAL MODULES NEEDED
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const app = express();
 
-
-// DATABASE CONNECTION TO MONGODB THROUGH ODM MONGOOSE
-mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true});
-
-  // VIEWS SET UP
-var exphds = require('express-handlebars');
-app.engine('handlebars', exphds({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-// IMPORT ROUTERS/CONTROLLERS
+// IMPORTING FILE MODULES
 const reviews = require('./controllers/reviews');
 const comments = require('./controllers/comments');
 const movies = require('./controllers/movies');
 const admin = require('./controllers/admin');
 
-  //MIDDLEWARE SET UP
+
+// DATABASE CONNECTION TO MONGODB THROUGH ODM MONGOOSE
+mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true});
+
+// VIEWS SET UP
+var exphds = require('express-handlebars');
+app.engine('handlebars', exphds({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//MIDDLEWARE SET UP
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(movies);
@@ -31,7 +32,7 @@ app.use(admin);
 app.use(express.static('public'));
 
 
-// SERVER LISTENING
+// SERVER BOOTING UP
 
 module.exports = app.listen(3000, () => {
   console.log('Server listening on Port 3000')
--- controllers/admin.js
+++ controllers/admin.js
@@ -7,23 +7,29 @@ const router = express.Router();
 // PATH TO REVIEW MODEL
 const Review = require('../models/review');
 
-// NEW COMMENT
+// ADMIN PAGE
 router.get('/admin', (req, res) => {
-    Review.find().then( reviews => {
-        res.render('admin', { reviews: reviews });
-    }).catch(err => {
-        console.log(err);
+    Review.find()
+        .then(reviews => {
+            res.render('admin', { reviews: reviews });
+     })
+        .catch(error => {
+        console.log(error.message);
+        });
+    });
+
+
+// ADMIND DELETE REVIEW
+router.delete('admin/reviews/:id', (req, res) => {
+    Review.findByIdAndDelete(req.params.id)
+    .then(review => {
+        res.status(200).send(review)
+    })
+    .catch(error => {;
+        console.log(error.message)
+        res.status(400).send(error);
     });
 });
 
-// DELETE COMMENT
-router.delete('/admin/delete/:id', (req, res) => {
-    Review.findByIdAndRemove(req.params.id).then(review => {
-        res.status(200).send(review)
-    }).catch((err) => {
-        console.log(err.message);
-        res.status(400).send(err)
-    });
-})
 module.exports = router;
 
